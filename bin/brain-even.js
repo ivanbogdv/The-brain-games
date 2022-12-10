ecfd19564fd95a770ecb198c9eb2e030d02a9d34
ivan.bogdv@gmail.com <ivan.bogdv@gmail.com>
2022-12-10 13:45:32 +0300

    Add brain-even.js and even-questions.js with part of game 'Even number'

---
+++ bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync'; // import readline-sync
+import questionAboutEven from '../scr/even-questions.js'; // import three question about even
+
+questionAboutEven();
