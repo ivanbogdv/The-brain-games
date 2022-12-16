bc23ebcfb221c62e99cef89def98bd7f5a84d174
ivan.bogdv@gmail.com <ivan.bogdv@gmail.com>
2022-12-16 16:48:57 +0300

    fix error

--- scr/games/calc-game.js
+++ scr/games/calc-game.js
@@ -2,24 +2,20 @@ import readlineSync from 'readline-sync'; // import readline-sync
 import randomNumbers from '../random_numbers.js'; // import random numbers
 import generalGameLogic from '../index.js'; // general game logic from index.js
 
+// rules of game
+const rules = 'What is the result of the expression?';
+
+// random const
+const singArr = ['+', '-', '*'];
+const randArr = Math.floor(Math.random() * singArr.length);
+const randomExpressionSign = singArr[randArr];
+
+const randomNumber1 = randomNumbers();
+const randomNumber2 = randomNumbers();
+
 const calcGame = () => {
-  // rules of game
-  const rules = 'What is the result of the expression?';
-
-  const calcTask = () => {
-    // random expression sign
-    const singArr = ['+', '-', '*'];
-    const randArr = Math.floor(Math.random() * singArr.length);
-    const randomExpressionSign = singArr[randArr];
-
-    // random numners
-    const randomNumber1 = randomNumbers();
-    const randomNumber2 = randomNumbers();
-
-    // task to user
-    const task = (`Question: ${randomNumber1} ${randomExpressionSign} ${randomNumber2}`);
-
-    // check the aswer
+  // calculate
+  const calculate = () => {
     let result = 0;
     switch (randomExpressionSign) {
       case '+':
@@ -34,17 +30,22 @@ const calcGame = () => {
       default:
         console.log('error');
     }
-    const correctAnswer = String(result);
+  };
+
+  // task and answer
+  const getQuestionAndAnswer = () => {
+    const task = (`Question: ${randomNumber1} ${randomExpressionSign} ${randomNumber2}`);
+    const correctAnswer = String(calculate);
     return [task, correctAnswer];
   };
 
-  generalGameLogic(rules, calcTask);
+  generalGameLogic(rules, getQuestionAndAnswer);
 };
 
 export default calcGame;
 
 // переменные которые нужно добавить в играх.
 // 1. rules +
-// 2. task +
+// 2. question +
 // 3. answerOfUser +
 // 4. correctAnswer +
